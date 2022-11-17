92de688c282df791226075bfae7953278f24e477
Sarah Mitko <sarahmitko@gmail.com>
2022-11-17 14:53:43 +0100

    seeded the database with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Movie.destroy_all
+
+10.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: Faker::LoremFlickr.image,
+    rating: rand(1..10)
+  )
+  movie.save!
+end
+puts 'finished!'
