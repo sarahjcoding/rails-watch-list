9648f8577c38843232f2ae923275d7c84893b58a
Sarah Mitko <sarahmitko@gmail.com>
2022-11-18 14:02:03 +0100

    added root to index in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:create,:new]
