bb4febea0aa646d2239b4ca87910acd0482461a3
Sarah Mitko <sarahmitko@gmail.com>
2022-11-18 17:10:20 +0100

    changes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,6 +17,8 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
     if @list.save
     redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
